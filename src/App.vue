c8de615b26aacba7d44a569183cfcd5ae84d7c32
Tanner Temple <tannerjtemple@gmail.com>
2026-03-02 10:28:27 -0600

    Event handling

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,102 @@
 <template>
   <h1>{{ message }}</h1>
-  <img v-bind:src="imageUrl" alt="" />
-  <br />
 
-  <!-- shorthand for v-bind -->
-  <img :src="imageUrl" alt="" />
-  <br />
+  <!-- replaceText is the name of a function defined in the script -->
+  <button v-on:click="replaceText('v-on is fun!')">Replace text</button>
 
-  <button @click="changeImg">Change image</button>
-
-  <br />
+  <!-- v-on:click shortens to @click -->
+  <button @click="replaceText('v-on is fun!')">Replace text</button>
 
   <hr />
 
-  <input type="text" :value="defaultInputText" />
+  <!-- using $event special variable to access native DOM event -->
+  <p>An input field where the user can ONLY enter numbers:</p>
+  <input type="text" @keydown="handleInput($event)" />
+
+  <hr />
+  <p>A demo of preventing the default behavior of an event.</p>
+  <p>Right click in the cyan box below will not show the context menu:</p>
+  <div
+    style="width: 100px; height: 100px; background-color: aqua"
+    @contextmenu.prevent="console.log('Show a custom context menu instead.')" 
+  ></div>
+  <!-- ^^^ prevents clicking the right mouse button from showing the default context menu. -->
 
   <hr />
 
-  <p :class="className">Harry Potter</p>
+  <p>A demo of stopping event propagation:</p>
 
-  <!-- define a JS object in :class -->
-  <p :class="{ inactive: isInactive, center: isCenter }">
-    <!-- if you think embedding a JS object in HTML is verbose, you can choose to move the object to the script, 
-        give it a name, and only put the JS object name in :class -->
-    Harry Potter
-  </p>
+  <div id="mouseover" @mouseover="fun1">
+    <textarea @mouseover.stop="fun2($event)">This is a text area.</textarea>
+  </div>
+  <!-- Since textarea is nested within div, mouseover textarea also means mouseover the parent div. -->
 
-  <!-- define a JS array in :class -->
-  <p :class="['active', 'center']">Harry Potter</p>
+  <hr />
+  <div>
+    Press down the "Enter" key will trigger a console log print:
+    <input
+      type="text"
+      @keydown.enter="console.log('You pressed the Enter key.')"
+    />
+  </div>
+  <div>
+    Press down the "Arrow Down" key will trigger a console log print:
+    <input
+      type="text"
+      @keydown.down="console.log('You pressed the Arrow Down key.')"
+    />
+  </div>
+  <div>
+    Press down the "Space" key will trigger a console log print:
+    <input
+      type="text"
+      @keydown.space="console.log('You pressed the Space key.')"
+    />
+  </div>
+  <div>
+    Press down the "b" key will trigger a console log print:
+    <input type="text" @keydown.b="console.log('You pressed the B key.')" />
+  </div>
+  <div>
+    Press down the "Ctrl c" will trigger a console log print:
+    <input type="text" @keydown.ctrl.c="console.log('You pressed Ctrl c.')" />
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-let message = 'Hello, v-bind!'
-let imageUrl = ref('public/img/banner_1.jpg')
+let message = ref('Hello, v-on!')
 
-function changeImg() {
-  imageUrl.value = 'public/img/banner_2.jpg'
+function replaceText(msg) {
+  message.value = msg
 }
 
-let defaultInputText = 'Write something here...'
+function handleInput(event) {
+  console.log(event)
+  // get the key code of the key pressed by user
+  let keyCode = event.keyCode
+  // if the key pressed is not a number key (0 - 9), don't show in the input box
+  if (keyCode < 48 || keyCode > 57) {
+    // vanilla JS code:
+    event.preventDefault()
+  }
+}
 
-let className = ref('active')
-let isInactive = ref(true)
-let isCenter = ref(false)
+function fun1() {
+  console.log('mouse over div')
+}
+
+function fun2(event) {
+  console.log('mouse over textarea')
+}
 </script>
 
 <style scoped>
-img {
-  max-width: 300px;
-}
-
-.active {
-  color: green;
-}
-
-.inactive {
-  color: red;
-  text-decoration: line-through;
-}
-
-.center {
-  text-align: center;
+#mouseover {
+  text-align: right;
+  background-color: purple;
+  width: 300px;
+  height: 300px;
 }
 </style>
