20a1c148919e8f24a5a79a95e14255e7898050bf
Tanner Temple <tannerjtemple@gmail.com>
2026-03-02 11:04:39 -0600

    Form input bindings

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,109 @@
 <template>
   <h1>{{ message }}</h1>
-  <img v-bind:src="imageUrl" alt="" />
-  <br />
-
-  <!-- shorthand for v-bind -->
-  <img :src="imageUrl" alt="" />
-  <br />
-
-  <button @click="changeImg">Change image</button>
-
-  <br />
-
-  <hr />
-
-  <input type="text" :value="defaultInputText" />
-
-  <hr />
-
-  <p :class="className">Harry Potter</p>
-
-  <!-- define a JS object in :class -->
-  <p :class="{ inactive: isInactive, center: isCenter }">
-    <!-- if you think embedding a JS object in HTML is verbose, you can choose to move the object to the script, 
-        give it a name, and only put the JS object name in :class -->
-    Harry Potter
-  </p>
-
-  <!-- define a JS array in :class -->
-  <p :class="['active', 'center']">Harry Potter</p>
+  <h1>Welcome, {{ formData.name }}</h1>
+  <form action="">
+    <div>
+      <span>Name:</span>
+      <span>
+        <!-- formData.name is synced after "change" event instead of "input" event. -->
+        <input type="text" v-model.lazy="formData.name" />
+      </span>
+    </div>
+    <div>
+      <span>Gender:</span>
+      <span>
+        <input type="radio" id="male" value="M" v-model="formData.gender" />
+        <label for="male">M</label>
+        <input type="radio" id="female" value="F" v-model="formData.gender" />
+        <label for="female">F</label>
+      </span>
+    </div>
+    <div>
+      <span>Age:</span>
+      <span>
+        <!-- Input is automatically typecast as a number. -->
+        <input type="text" id="age" v-model.number="formData.age" />
+      </span>
+    </div>
+    <div>
+      <span>Hobbies:</span>
+      <!-- We can bind multiple checkboxes to the same array. -->
+      <input type="checkbox" value="basketball" v-model="formData.hobbies" />
+      <label for="basketball">Basketball</label>
+      <input type="checkbox" value="football" v-model="formData.hobbies" />
+      <label for="football">Football</label>
+      <input type="checkbox" value="js" v-model="formData.hobbies" />
+      <label for="js">JavaScript</label>
+    </div>
+    <div>
+      <span>Profession:</span>
+      <select v-model="formData.profession">
+        <option value="">Please select one</option>
+        <option value="1">Software Engineer</option>
+        <option value="2">Data Scientist</option>
+        <option value="3">Product Manager</option>
+        <option value="4">Professor</option>
+      </select>
+    </div>
+    <div>
+      <span>Description:</span>
+      <!-- Leading and trailing whitespace is removed automatically. -->
+      <textarea
+        v-model.trim="formData.desc"
+        @keyup.enter="submitForm"
+      ></textarea>
+    </div>
+    <div>
+      <input type="reset" value="Reset" @click="resetForm" />
+      <!-- Prevent the form from submitting through form action. -->
+      <input type="submit" value="Submit" @click.prevent="submitForm" />
+    </div>
+  </form>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-let message = 'Hello, v-bind!'
-let imageUrl = ref('public/img/banner_1.jpg')
+let message = ref('Hello, v-model!')
 
-function changeImg() {
-  imageUrl.value = 'public/img/banner_2.jpg'
+let formData = ref({
+  name: '',
+  gender: '',
+  age: '',
+  hobbies: [],
+  profession: '',
+  desc: ''
+})
+
+function resetForm() {
+  formData.value = {
+    name: '',
+    gender: '',
+    age: '',
+    hobbies: [],
+    profession: '',
+    desc: ''
+  }
 }
 
-let defaultInputText = 'Write something here...'
-
-let className = ref('active')
-let isInactive = ref(true)
-let isCenter = ref(false)
+function submitForm() {
+  console.log(JSON.stringify(formData.value))
+  // We can use axios or fetch() to submit data to the back end. for now it just shows in console
+}
 </script>
 
 <style scoped>
-img {
-  max-width: 300px;
+form {
+  padding: 20px;
 }
 
-.active {
-  color: green;
+form div {
+  height: 40px;
+  line-height: 40px;
 }
 
-.inactive {
-  color: red;
-  text-decoration: line-through;
-}
-
-.center {
-  text-align: center;
+form div span {
+  display: inline-block;
+  width: 100px;
 }
 </style>
