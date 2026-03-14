32b2ae55439956f2f0ca25fe26132c6e84ce8b3a
Tanner Temple <tannerjtemple@gmail.com>
2026-03-13 20:52:14 -0400

    Vue named slots

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,20 @@
 <template>
-  <h1>{{ message }}</h1>
-  <img v-bind:src="imageUrl" alt="" />
-  <br />
-
-  <!-- shorthand for v-bind -->
-  <img :src="imageUrl" alt="" />
-  <br />
-
-  <button @click="changeImg">Change image</button>
-
-  <br />
-
-  <hr />
-
-  <input type="text" :value="defaultInputText" />
-
-  <hr />
-
-  <p :class="className">Harry Potter</p>
-
-  <!-- define a JS object in :class -->
-  <p :class="{ inactive: isInactive, center: isCenter }">
-    <!-- if you think embedding a JS object in HTML is verbose, you can choose to move the object to the script, 
-        give it a name, and only put the JS object name in :class -->
-    Harry Potter
-  </p>
-
-  <!-- define a JS array in :class -->
-  <p :class="['active', 'center']">Harry Potter</p>
+  <BaseLayout>
+    <template #header>
+      <h1>Here might be a page title.</h1>
+    </template>
+    <template #main>
+      <p>A paragraph of the main content.</p>
+      <p>And another one.</p>
+    </template>
+    <template #footer>
+      <p>Here's some contact info.</p>
+    </template>
+  </BaseLayout>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-let message = 'Hello, v-bind!'
-let imageUrl = ref('public/img/banner_1.jpg')
-
-function changeImg() {
-  imageUrl.value = 'public/img/banner_2.jpg'
-}
-
-let defaultInputText = 'Write something here...'
-
-let className = ref('active')
-let isInactive = ref(true)
-let isCenter = ref(false)
+import BaseLayout from './BaseLayout.vue'
 </script>
 
-<style scoped>
-img {
-  max-width: 300px;
-}
-
-.active {
-  color: green;
-}
-
-.inactive {
-  color: red;
-  text-decoration: line-through;
-}
-
-.center {
-  text-align: center;
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/BaseLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+      <!--  header content here -->
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <!-- main content here -->
+      <slot name="main"></slot>
+    </main>
+    <footer>
+      
+        <!-- footer content here-->
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
